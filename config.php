0d5892ddea9801cc6a57859a00b381349784dcaa
akkotto <akkotto@mail.ri>
2017-05-27 01:05:01 +0300

    1.0.0

--- config.php
+++ config.php
@@ -1,17 +1,17 @@
 <?php
 	/* Подключение к базе данных */
 	$db = array(
-		"host" => "", //Хост Mysql
-		"user" => "", //Пользователь Mysql
-		"pass" => "", //Пароль от пользователя mysql
-		"name" => "" //Название Базы Данных Mysql
+		"host" => "localhost", //Хост Mysql
+		"user" => "root", //Пользователь Mysql
+		"pass" => "MSADKlfmsjafjasfmakslfjkla", //Пароль от пользователя mysql
+		"name" => "test" //Название Базы Данных Mysql
 	);
-	$db = mysql_connect($db['host'], $db['user'], $db['pass']);
-	mysql_select_db($db['name']);
+	$db2 = mysql_connect($db['host'], $db['user'], $db['pass']);
+	mysql_select_db($db['name'], $db2);
 	/* Настройка авторизации черещз steam */
-	$apikey = ""; // Сгенерировать можно тут http://steamcommunity.com/dev/apikey
-	$domain = ""; // Домен сайта
-	$redirectpage = "/"; // Куда отправляем человека после авторизации
+	$apikey = "27E189F854DE61F418A4B320A83E9EC0"; // Сгенерировать можно тут http://steamcommunity.com/dev/apikey
+	$domain = "http://moscowgaming.ru"; // Домен сайта
+	$redirectpage = "/test/"; // Куда отправляем человека после авторизации
 	/* Найтройка данных от платежной системы */
 	$shopid = ""; // ID Вашего магазина на сервисе Free-kassa.ru
 	$secret = ""; // Секретный ключ
