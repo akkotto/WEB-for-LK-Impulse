888a7e6220b2d73e043cee1254ed33cbe02b662e
akkotto <kasiaksa@gmail.com>
2017-05-27 01:05:36 +0300

    Update config.php

--- config.php
+++ config.php
@@ -9,9 +9,9 @@
 	$db2 = mysql_connect($db['host'], $db['user'], $db['pass']);
 	mysql_select_db($db['name'], $db2);
 	/* Настройка авторизации черещз steam */
-	$apikey = "27E189F854DE61F418A4B320A83E9EC0"; // Сгенерировать можно тут http://steamcommunity.com/dev/apikey
-	$domain = "http://moscowgaming.ru"; // Домен сайта
-	$redirectpage = "/test/"; // Куда отправляем человека после авторизации
+	$apikey = ""; // Сгенерировать можно тут http://steamcommunity.com/dev/apikey
+	$domain = ""; // Домен сайта
+	$redirectpage = "/"; // Куда отправляем человека после авторизации
 	/* Найтройка данных от платежной системы */
 	$shopid = ""; // ID Вашего магазина на сервисе Free-kassa.ru
 	$secret = ""; // Секретный ключ
