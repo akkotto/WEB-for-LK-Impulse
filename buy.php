887e80f5287779bb1d577c21d5e4d43efa1fad71
akkotto <akkotto@mail.ri>
2017-06-03 13:19:52 +0300

    1.0.0
    Fixed bug with steamid

--- buy.php
+++ buy.php
@@ -9,7 +9,8 @@
 			die("Вы ввели некорректную сумму пополнения");
 		}
 		$summ = (int)$_POST['summ'];
-		$sql = mysql_query("INSERT INTO `buy`(`id`, `steamid`, `summ`) VALUES ('','".mysql_real_escape_string($_POST['steamid'])."','".$summ."')");
+		$steamid = str_replace("STEAM_0", "STEAM_1", $_POST['steamid']);
+		$sql = mysql_query("INSERT INTO `buy`(`id`, `steamid`, `summ`) VALUES ('','".mysql_real_escape_string($steamid)."','".$summ."')");
 		echo "<script language='JavaScript'> window.location.href = 'http://www.free-kassa.ru/merchant/cash.php?m=".$shopid."&oa=".$summ."&o=".mysql_insert_id()."&s=".md5($shopid.':'.$summ.':'.$secret.':'.mysql_insert_id())."'; </script>";
 	}else{
 		die('lol');
